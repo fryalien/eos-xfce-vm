b1c4471b06340f23505f3118a0507fa7cbe83546
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2026-01-24 19:07:19 +0100

    y

--- eos-xfce-install.sh
+++ eos-xfce-install.sh
@@ -62,7 +62,7 @@ echo "##### COPY XFCE CONFIG #####"
 
 cp -rfv ./xfce-config/xfce4 ~/.config/
 cp -rfv ./xfce-config/Thunar ~/.config/
-cp -rfv ./xfce-config/gth-3.0 ~/.config/
+cp -rfv ./xfce-config/gtk-3.0 ~/.config/
 
 echo "##### LOAD vm PANEL CONFIG #####"
 
