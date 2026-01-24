f22baad0b53460f35eb13929c548914d5348c54b
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2026-01-24 19:04:10 +0100

    y

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "##### SCRIPT STARTED #####"
+
+date
+
+######################################################################################
+
 # INSTALL EVERYTHING AND MAKE A LOG OF IT
 
 log="./install-$(date +%Y-%m-%d_%H:%M).log"
@@ -21,3 +27,9 @@ xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/presentation-mode -s
 echo "##### START INSTALLATION #####"
 
 ./eos-xfce-install.sh  | tee -a "$log"
+
+######################################################################################
+
+echo "##### SCRIPT ENDED #####"
+
+date
