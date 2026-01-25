e589d496a9a8b11aee065d789d3cd03274f37b80
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2026-01-25 20:34:02 +0100

    y

--- scripts/flatpak-config.sh
+++ scripts/flatpak-config.sh
@@ -5,7 +5,7 @@
 mkdir -p ~/.var/app
 
 # Copy apps configs.
-unzip -o ../flatpak-var-app/com.brave.Browser.zip -d ~/.var/app
-unzip -o ../flatpak-var-app/org.flameshot.Flameshot.zip -d ~/.var/app
-unzip -o ../flatpak-var-app/org.geany.Geany.zip -d ~/.var/app
-unzip -o ../flatpak-var-app/org.localsend.localsend_app.zip -d ~/.var/app
+unzip -o ./flatpak-var-app/com.brave.Browser.zip -d ~/.var/app
+unzip -o ./flatpak-var-app/org.flameshot.Flameshot.zip -d ~/.var/app
+unzip -o ./flatpak-var-app/org.geany.Geany.zip -d ~/.var/app
+unzip -o ./flatpak-var-app/org.localsend.localsend_app.zip -d ~/.var/app
