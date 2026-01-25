2b018a293e69b4e02431631d34da206d6b84b06e
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2026-01-25 19:29:48 +0100

    Create flatpak-config.sh

---
+++ scripts/flatpak-config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Make flapak config dirf
+
+mkdir -p ~/.var/app
+
+# Copy apps configs.
+
+unzip -v \
+			'./flatpak-var-apps/com.brave.Browser.zip' \
+      './flatpak-var-apps/org.flameshot.Flameshot.zip' \
+      './flatpak-var-apps/org.keepassxc.KeePassXC.zip' \
+      './flatpak-var-apps/org.geany.Geany.zip' \
+      './flatpak-var-apps/org.localsend.localsend_app.zip' \
+      -d ~/.var/apps/
