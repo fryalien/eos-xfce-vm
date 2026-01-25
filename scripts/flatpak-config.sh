b9816b581f4187571d9d4e8d69f6bdba667b1f5b
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2026-01-25 20:20:42 +0100

    y

--- scripts/flatpak-config.sh
+++ scripts/flatpak-config.sh
@@ -8,6 +8,4 @@ mkdir -p ~/.var/app
 unzip -o ../flatpak-var-app/com.brave.Browser.zip -d ~/.var/app
 unzip -o ../flatpak-var-app/org.flameshot.Flameshot.zip -d ~/.var/app
 unzip -o ../flatpak-var-app/org.geany.Geany.zip -d ~/.var/app
-unzip -o ../flatpak-var-app/org.keepassxc.KeePassXC.zip -d ~/.var/app
 unzip -o ../flatpak-var-app/org.localsend.localsend_app.zip -d ~/.var/app
-unzip -o ../flatpak-var-app/org.qbittorrent.qBittorrent.zip -d ~/.var/app
